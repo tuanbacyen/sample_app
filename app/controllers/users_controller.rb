3596ea8d4fe1a243f3aa923fbf466993f166f0c6
Tuan Pham <tuanbacyen@gmail.com>
2018-01-11 09:36:59 +0700

    end chapter sign up
    finish chapter 7: sign up
    
    fix cmt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,27 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id])
     if @user.nil?
-      flash[:danger] = t ".notfound"
+      flash[:success] = t("controllers.notfound")
       redirect_to root_path
     end
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t("controllers.welcome")
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
